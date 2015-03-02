20a4fc6d08dbe793783911077de003cc250ba72f
Franz Wilding <franz@2robots.at>
2015-03-02 17:15:57 +0100

    drafting basic styles

---
+++ Resources/public/js/one.js
@@ -0,0 +1,75 @@
+/**
+ * A small wrapper object for custom functions.
+ */
+var UnitedOne = {
+
+  /**
+   * Holds all modules
+   */
+  modules: {},
+
+  /**
+   * Call init function on all modules.
+   */
+  init: function(){
+    for(var module in this.modules) {
+      if(this.modules[module].hasOwnProperty('init')) {
+        this.modules[module].init();
+      }
+    }
+  },
+
+  /**
+   * Call ready functions on all modules.
+   */
+  ready: function(){
+    for(var module in this.modules) {
+      if(this.modules[module].hasOwnProperty('ready')) {
+        this.modules[module].ready();
+      }
+    }
+  }
+};
+
+// Call init function
+UnitedOne.init();
+
+// Call ready function
+$(document).ready(function(){
+  UnitedOne.ready();
+});
+
+
+/**
+ * Toggle sidebar navigation.
+ */
+UnitedOne.modules.toggleSidebar = {
+
+  sidebar: $('.sidebar'),
+  toggler: $('.toggle-sidebar'),
+  header:  $('.united-one-header'),
+
+  ready: function(){
+    var t = this;
+
+    t.toggler.click(function(){
+      t.sidebar.sidebar('setting', {
+        transition:     'overlay',
+        dimPage:        false,
+        pushable:       false,
+        onChange:       function(){
+          t.header.toggleClass('open');
+        }
+
+      }).sidebar('toggle');
+    });
+  }
+};
+
+UnitedOne.modules.select = {
+  ready: function(){
+    $('.ui.dropdown')
+      .dropdown()
+    ;
+  }
+};
