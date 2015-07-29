881d6b6febb12ef914ab25fee87863e21b3ca5a2
Franz Wilding <franz@2robots.at>
2015-07-29 15:28:28 +0200

    add cancel_url option to the delete form type

--- Form/Type/DeleteType.php
+++ Form/Type/DeleteType.php
@@ -3,6 +3,9 @@
 namespace United\OneBundle\Form\Type;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class DeleteType extends SubmitType
 {
@@ -10,6 +13,29 @@ class DeleteType extends SubmitType
     public function getParent()
     {
         return 'submit';
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        if($options['cancel_url']) {
+            $view->vars['cancel_url'] = $options['cancel_url'];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setDefaults(
+          array(
+            'cancel_url'        => null,
+          )
+        );
     }
 
     public function getName()
